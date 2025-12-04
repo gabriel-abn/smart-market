80ea735ac893e2479f899b46f55a5688f56919a2
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2025-12-04 17:04:12 -0300

    chore: remove unnecessary Firebase initialization comments.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO: Substitua com suas credenciais do Firebase
 const firebaseConfig = {
 	apiKey: "AIzaSyDJSH9ab-sQXuUjJN_t1I7N0hJYvuZ10fU",
 	authDomain: "lista-compra-407be.firebaseapp.com",
@@ -11,8 +10,6 @@ const firebaseConfig = {
 	appId: "1:440080728486:web:89035dc0bfc8931ff7cf67"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-// Inicializa o Firestore
 export const db = getFirestore(app);
