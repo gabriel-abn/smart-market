781fffe75fe6f4f27d58e85360f0aa1416443d42
Gabriel Antônio <gabriel.ab.nascimento@gmail.com>
2025-12-09 08:57:50 -0300

    Remove sensitive data from firebaseConfig.js
    Removed sensitive Firebase configuration details.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,12 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDJSH9ab-sQXuUjJN_t1I7N0hJYvuZ10fU",
-	authDomain: "lista-compra-407be.firebaseapp.com",
-	projectId: "lista-compra-407be",
-	storageBucket: "lista-compra-407be.firebasestorage.app",
-	messagingSenderId: "440080728486",
-	appId: "1:440080728486:web:89035dc0bfc8931ff7cf67"
+
 };
 
 const app = initializeApp(firebaseConfig);
